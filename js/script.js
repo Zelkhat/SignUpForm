55b3a210b489af7dbbad1e00a18127a813a6155d
Zelkhat <zelkhat@gmail.com>
2017-06-16 00:07:45 -0400

    delete class incorrect then Reset

--- js/script.js
+++ js/script.js
@@ -1,11 +1,20 @@
 var incorrectTags = [];
 
+window.onload = function() {
+
+	// add an action on the reset button
+	document.querySelector('input[type=reset]').onclick = function() {
+		deleteIncorrect();
+	}
+}
+
 function validateForm() {
 	
 	var textInputs = document.querySelectorAll('input[type=text], input[type=password]');
 	/*var emailInput = document.querySelector('input[name=email]');*/
 	var genderInputs = document.querySelectorAll('.gender input[type=radio]');
 	/*var passInputs = document.querySelectorAll('input[type=password]');*/
+	// set the array to zero
 	incorrectTags.length = 0;
 
 	// check all text inputs, they should not be empty
@@ -14,8 +23,10 @@ function validateForm() {
 	genderIsEmpty(genderInputs);
 	
 	paintInRed();
-	return false;
+
 	alert('Everything is OK! You got it!');
+	return false;
+	
 }
 
 // add class .incorrect to each element of the array incorrectTags
@@ -63,5 +74,15 @@ function genderIsEmpty(inputs) {
 
 // delete class .incorrect from all tags
 function deleteIncorrect() {
+	
+	var incorrectClassTags = document.querySelectorAll('.incorrect');
+	var i = 0;
 
+	for (var i = 0; i < incorrectClassTags.length; i++) {
+		incorrectClassTags[i].classList.remove('incorrect');
+	}
+	
+	// set the array [incorrectTags] to zero
+	incorrectTags.length = 0;
 }
+
